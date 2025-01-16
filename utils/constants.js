83d914f7f312b1f6a2a596f27647dfe1b51a9e1d
Josh Kelly <jkelly85@alumni.uwo.ca>
2025-01-15 22:08:03 -0500

    add constants

---
+++ utils/constants.js
@@ -0,0 +1,23 @@
+// constants.js
+
+// Maximum number of dice per player at the start
+export const MAX_DICE_PER_PLAYER = 5;
+
+// Total number of faces on a die
+export const DIE_FACES = 6;
+
+// Wild face value (e.g., 1 is often considered wild)
+export const WILD_FACE = 1;
+
+// Maximum number of players allowed
+export const MAX_PLAYERS = 6;
+
+// Minimum number of players allowed
+export const MIN_PLAYERS = 2;
+
+// Messages for common actions (can be expanded for localization)
+export const MESSAGES = {
+  INVALID_BID: "Your bid is invalid! Please bid higher or challenge.",
+  GAME_OVER: "The game is over!",
+  PLAYER_ELIMINATED: "A player has been eliminated!",
+};
